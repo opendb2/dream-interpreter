b7e69f9e0b3cb62e89fa714637cb2bbc488df3e5
Jibril <open_db2@163.com>
2024-07-07 22:14:39 +0800

    添加 gen 页面图片

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/home.vue'
+import Gen from '@/views/gen.vue'
+import Chat from '@/views/chat.vue'
+import Jour from '@/views/journal.vue'
+import Search from '@/views/search.vue'
+import Tour from '@/views/tour.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +14,11 @@ const router = createRouter({
       name: 'home',
       component: Home
     },
+	{ path: '/gen', name: 'gen', component: Gen },
+	{ path: '/chat', name: 'chat', component: Chat },
+	{ path: '/jour', name: 'jour', component: Jour },
+	{ path: '/search', name: 'search', Search },
+	{ path: '/tour', name: 'tour', Tour },
   ]
 })
 
--- src/views/gen.vue
+++ src/views/gen.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>gen</div>
+  <div class="page-gen">
+	  <input class="gen-text" type="text" placeholder="what was your last dream?"/>
+  </div>
+  <div class='illustraion-1'></div>
+  <div class='illustraion-2'></div>
+  <div class='illustraion-3'></div>
+  <div class='illustraion-4'></div>
+  <div class='illustraion-5'></div>
+  <div class='illustraion-6'></div>
+  <div class='illustraion-7'></div>
 </template>
 
 <script setup>
@@ -7,12 +16,82 @@ console.log('gen');
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.page-gen {
+	flex: 1;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.gen-text {
+	width: 60vw;
+	height: 10vh;
+	font-size: 3vh;
+	line-height: 3vh;
+	border-radius: 5vh;
+	padding: 5vh;
+}
+.illustraion-1 {
+	position: absolute;
+	top: 20vh;
+	left: 13vw;
+	width: 7vw;
+	height: 7vw;
+	background-image: url('../assets/logo.png');
+	background-size: cover;
+}
+.illustraion-2 {
+	position: absolute;
+	top: 25vh;
+	left: 44vw;
+	width: 7vw;
+	height: 7vw;
+	background-image: url('../assets/logo.png');
+	background-size: cover;
+}
+.illustraion-3 {
+	position: absolute;
+	top: 22vh;
+	left: 75vw;
+	width: 10vw;
+	height: 10vw;
+	background-image: url('../assets/logo.png');
+	background-size: cover;
+}
+.illustraion-4 {
+	position: absolute;
+	top: 55vh;
+	left: 6vw;
+	width: 6vw;
+	height: 6vw;
+	background-image: url('../assets/logo.png');
+	background-size: cover;
+	transform: rotate(-7deg);
+}
+.illustraion-5 {
+	position: absolute;
+	top: 80vh;
+	left: 15vw;
+	width: 7vw;
+	height: 7vw;
+	background-image: url('../assets/logo.png');
+	background-size: cover;
+}
+.illustraion-6 {
+	position: absolute;
+	top: 78vh;
+	left: 52vw;
+	width: 7vw;
+	height: 7vw;
+	background-image: url('../assets/logo.png');
+	background-size: cover;
+}
+.illustraion-7 {
+	position: absolute;
+	top: 62vh;
+	left: 85vw;
+	width: 10vw;
+	height: 10vw;
+	background-image: url('../assets/logo.png');
+	background-size: cover;
 }
 </style>
