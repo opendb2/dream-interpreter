d1e358e4b2ff697e60f13fb866920eb2f57f6e37
Mengying Fang <week.fang@gmail.com>
2024-07-24 16:23:28 +0100

    modify port

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:7001',
+        target: 'http://127.0.0.1:7000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
